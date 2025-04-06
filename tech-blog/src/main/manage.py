ade7f1454c23f0b0510deb751e2de4cd982d3677
pro-grammer-SD <geniussantu1983@gmail.com>
2025-04-06 17:18:35 +0530

    UP

--- tech-blog/src/main/manage.py
+++ tech-blog/src/main/manage.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
-import os
 import sys
+import os
 
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 def main():
     """Run administrative tasks."""
@@ -16,7 +18,6 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
-
 
 if __name__ == '__main__':
     main()
